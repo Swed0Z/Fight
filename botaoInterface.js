ba57ea930eb1419d6f5d0d537df409be7eae139f
Swed0Z <arrowdaamazonia@gmail.com>
2024-01-08 00:35:09 -0300

    .

--- botaoInterface.js
+++ botaoInterface.js
@@ -1,1 +1,7 @@
-let
+function Play {
+    let interface1 = document.querySelector('.interface1')
+    let interface2 = document.querySelector('.interface2')
+
+    interface1.classList.remove('')
+    interface2.classList.add ('')
+}
