c5dc6d625112de4b1e133f9104c6921fac7f0abb
Swed0Z <arrowdaamazonia@gmail.com>
2024-01-14 18:32:02 -0300

    .

--- classes.js
+++ classes.js
@@ -6,18 +6,18 @@ class Character {
     defense = 0
     attack = 0
 
-    constructor(name){
-    this.name = name
+    constructor(name) {
+        this.name = name
     }
-    get life(){
+    get life() {
         return this._life
     }
-    set life(newLife){
+    set life(newLife) {
         this._life = newLife < 0 ? 0 : newLife
     }
 }
-class Knight extends Character{
-    constructor(name){
+class Knight extends Character {
+    constructor(name) {
         super(name)
         this.life = 100
         this.attack = 10
@@ -25,8 +25,8 @@ class Knight extends Character{
         this.maxLife = this.life
     }
 }
-class Mage extends Character{
-    constructor(name){
+class Mage extends Character {
+    constructor(name) {
         super(name)
         this.life = 60
         this.attack = 25
@@ -34,23 +34,23 @@ class Mage extends Character{
         this.maxLife = this.life
     }
 }
-class LittleMonster extends Character{
-    constructor(){
-    super('Little Monster')
-    this.life = 30
-    this.attack = 5
-    this.defense = 2
-    this.maxLife = this.life
+class LittleMonster extends Character {
+    constructor() {
+        super('Little Monster')
+        this.life = 30
+        this.attack = 5
+        this.defense = 2
+        this.maxLife = this.life
+    }
 }
-}
-class BigMonster extends Character{
-    constructor(){
-    super('Big Monster')
-    this.life = 120
-    this.attack = 15
-    this.defense = 1
-    this.maxLife = this.life
-}
+class BigMonster extends Character {
+    constructor() {
+        super('Big Monster')
+        this.life = 120
+        this.attack = 15
+        this.defense = 1
+        this.maxLife = this.life
+    }
 }
 class Stage {
     constructor(Fighter1, Fighter2, FighterEL1, FighterEL2) {
@@ -61,51 +61,51 @@ class Stage {
     }
 
     start() {
-        update()
-        this.FighterEL1.querySelector('.AttackButton').addEventListener('click', () => this.doAttack(this.Fighter1,this.Fighter2))
 
-        this.FighterEL2.querySelector('.AttackButton').addEventListener('click', () => this.doAttack(this.Fighter2,this.Fighter1))
-        
+        this.loadGame();
+        this.update();
+        this.FighterEL1.querySelector('.AttackButton').addEventListener('click', () => this.doAttack(this.Fighter1, this.Fighter2))
+
+        this.FighterEL2.querySelector('.AttackButton').addEventListener('click', () => this.doAttack(this.Fighter2, this.Fighter1))
     }
 
     update() {
-
-        let f1PCT = (this.Fighter1.life / this.Fighter1.maxLife) * 100
-        this.FighterEL1.querySelector('.name').innerHTML = `${this.Fighter1.name} - ${this.Fighter1.life.toFixed(1)}HP`
+        let f1PCT = (this.Fighter1._life / this.Fighter1.maxLife) * 100
+        this.FighterEL1.querySelector('.name').innerHTML = `${this.Fighter1.name} - ${this.Fighter1._life.toFixed(1)}HP`
         this.FighterEL1.querySelector('.Bar').style.width = `${f1PCT}%`
 
-        let f2PCT = (this.Fighter2.life / this.Fighter2.maxLife) * 100
-        this.FighterEL2.querySelector('.name').innerHTML = `${this.Fighter2.name} - ${this.Fighter2.life.toFixed(1)}HP`
+        let f2PCT = (this.Fighter2._life / this.Fighter2.maxLife) * 100
+        this.FighterEL2.querySelector('.name').innerHTML = `${this.Fighter2.name} - ${this.Fighter2._life.toFixed(1)}HP`
         this.FighterEL2.querySelector('.Bar').style.width = `${f2PCT}%`
-        
     }
-    doAttack(attacking, attacked){
-       if(attacking.life <= 0 || attacked.life <= 0){
+    doAttack(attacking, attacked) {
+        let attackFactor = (Math.random() * 2).toFixed(2)
+        let defenseFactor = (Math.random() * 2).toFixed(2)
+
+        let actualAttack = attacking.attack * attackFactor
+        let actualDefense = attacked.defense * defenseFactor
+
+        if (actualAttack > actualDefense) {
+            attacked._life -= actualAttack
+            console.log(`${attacking.name} acaba de causa ${actualAttack.toFixed(2)} em ${attacked.name}`)
+        } else {
+            console.log('defesa perfeita')
+
+        }
+
+        if (attacking._life <= 0 || attacked._life <= 0) {
             console.log('alguém ta morto')
             return
-       }
-       let attackFactor = (Math.random() * 2).toFixed(2)
-       let defenseFactor = (Math.random() * 2).toFixed(2)
-
-       let actualAttack = attacking.attack * attackFactor
-       let actualDefense = attacked.defense * defenseFactor
-
-       if (actualAttack > actualDefense) {
-            attacked.life -= actualAttack
-            console.log(`${attacking.name} acaba de causa ${actualAttack.toFixed(2)} em ${attacked.name}`)
-       } else {
-        console.log('defesa perfeita')
-
-       }
+        }
 
         this.update()
         this.saveGame()
     }
-    saveGame(){
+    saveGame() {
         localStorage.setItem('Fighter1', JSON.stringify(this.Fighter1));
         localStorage.setItem('Fighter2', JSON.stringify(this.Fighter2));
     }
-    loadGame(){
+    loadGame() {
         this.Fighter1 = JSON.parse(localStorage.getItem('Fighter1'));
         this.Fighter2 = JSON.parse(localStorage.getItem('Fighter2'));
     }
