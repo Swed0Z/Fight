d566d328e926a7ee2b559dd69a2081d3c588534d
Swed0Z <arrowdaamazonia@gmail.com>
2024-01-18 17:47:03 -0300

    .

--- botaoInterface.js
+++ botaoInterface.js
@@ -1,29 +1,26 @@
+let interface1 = document.querySelector('.interface1')
+let interface2 = document.querySelector('.interface2')
+let initial = document.querySelector('.initial')
+let fightArea = document.querySelector('.FightArea')
+let selectHero = document.querySelector('.selectHero')
+let nameHero = document.querySelector('.nameHeroArea')
 function Play() {
-    let interface1 = document.querySelector('.interface1')
-    let interface2 = document.querySelector('.interface2')
-
     interface2.classList.remove('hidden')
     interface1.classList.add ('hidden')
 }
 function FightArea(){
-    let initial = document.querySelector('.initial')
-    let FightArea = document.querySelector('.FightArea')
-
-    FightArea.classList.remove('hidden')
+    fightArea.classList.remove('hidden')
     initial.classList.add ('hidden')
 }
-function selectHeroArea(){
-    let initial = document.querySelector('.initial')
-    let selectHero = document.querySelector('.selectHero')
+function nameHeroArea(){
 
+}
+function selectHeroArea(){
     selectHero.classList.remove('hidden')
     initial.classList.add ('hidden')
 }
-function FightArea2(){
-    let FightArea = document.querySelector('.FightArea')
-    let selectHero = document.querySelector('.selectHero')
-    
-    FightArea.classList.remove('hidden')
+function FightArea2(){    
+    fightArea.classList.remove('hidden')
     selectHero.classList.add ('hidden')
 }
 document.querySelector('#beginning').addEventListener('click', function() {
